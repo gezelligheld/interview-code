f83a342261467d2fe1e95241be854157a14f6ef5
毛佳庆 <1970173678@qq.com>
2023-11-30 17:08:37 +0800

    update

--- src/inherit.ts
+++ src/inherit.ts
@@ -1,3 +1,12 @@
 /**
  * 继承
  */
+function inherit(child: Function, parent: Function) {
+  // 继承父类原型，相当于空的中间类
+  const parentPrototype = Object.create(parent.prototype);
+  // 子类原型的属性可以覆盖父类
+  child.prototype = Object.assign(parentPrototype, child.prototype);
+  child.prototype.constructor = child;
+}
+
+export default inherit;
--- src/promiseLimit.ts
+++ src/promiseLimit.ts
@@ -26,6 +26,9 @@ function loadImage(url: { info: string; time: number }) {
   });
 }
 
+/**
+ * 利用Promise.race
+ */
 function promiseLimit(
   data: any[],
   handle: (...args: any[]) => Promise<any>,
@@ -43,6 +46,34 @@ function promiseLimit(
   }
 }
 
-console.log(promiseLimit(urls, loadImage, 3));
+// promiseLimit(urls, loadImage, 3);
 
-export default promiseLimit;
+function promiseLimit2(list: Promise<any>[], limit: number) {
+  function handle(list: Promise<any>[], limit: number) {
+    if (!list.length) {
+      return;
+    }
+    const num = Math.min(list.length, limit);
+    if (num === 0) {
+      return;
+    }
+    for (let i = 0; i < num; i++) {
+      const fn = list.shift();
+      // 容量limit，执行一个后空间减1
+      limit--;
+      fn?.then(() => {
+        limit++;
+        // 容量limit，执行完一个后空间加1
+        handle(list, limit);
+      });
+    }
+  }
+  handle(list, limit);
+}
+
+promiseLimit2(
+  urls.map((u) => loadImage(u)),
+  3
+);
+
+export { promiseLimit, promiseLimit2 };
